fba8764564cad3b532769a556a344df8cb84ec21
Ricky Brown <ricky@brilliantlabs.co>
2021-02-11 10:28:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ let nodes = document.getElementsByTagName("link");
 let favicons = [...nodes].filter(link => link.getAttribute("rel") === "icon");
 
 favicons.forEach(favicon => favicon.setAttribute("href", newIcon))
+
+
+// Change "Listen for free" text
+document.querySelector('.follow-this-podcast h4').innerText = 'Subscribe to this podcast'
